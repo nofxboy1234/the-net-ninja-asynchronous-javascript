7998dc1cc26287bd53ec6b9f99b857ccb4f1bb86
Dylan Palmboom <nofxboy1234@gmail.com>
2022-09-14 15:10:09 +0200

    Use Fetch API

--- index.js
+++ index.js
@@ -37,6 +37,25 @@ function testPromises() {
     });
 }
 
+function testChainPromises() {
+  getTodos('todos/luigi.json')
+    .then((data) => {
+      console.log('promise 1 resolved:', data);
+      return getTodos('todos/mario.json');
+    })
+    .then((data) => {
+      // Fired when 'todos/mario.json is complete
+      console.log('promise 2 resolved:', data);
+      return getTodos('todos/shaun.json');
+    })
+    .then((data) => {
+      console.log('promise 3 resolved:', data);
+    })
+    .catch((err) => {
+      console.log('promise rejected:', err);
+    });
+}
+
 const getTodos = (resource) => {
   return new Promise((resolve, reject) => {
     const request = new XMLHttpRequest();
@@ -55,19 +74,14 @@ const getTodos = (resource) => {
   });
 };
 
-getTodos('todos/luigi.json')
-  .then((data) => {
-    console.log('promise 1 resolved:', data);
-    return getTodos('todos/mario.json');
+fetch('todos/luigi.json')
+  .then((response) => {
+    console.log('resolved', response);
+    return response.json();
   })
   .then((data) => {
-    // Fired when 'todos/mario.json is complete
-    console.log('promise 2 resolved:', data);
-    return getTodos('todos/shaun.json');
-  })
-  .then((data) => {
-    console.log('promise 3 resolved:', data);
+    console.log(data);
   })
   .catch((err) => {
-    console.log('promise rejected:', err);
+    console.log('rejected', err);
   });
