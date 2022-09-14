7d98172bb87cc577c863b5dbf2713bcacaf75158
Dylan Palmboom <nofxboy1234@gmail.com>
2022-09-14 11:36:38 +0200

    Use Promise to work with asynchronous code, instead of callbacks

--- index.js
+++ index.js
@@ -37,19 +37,28 @@ function testPromises() {
     });
 }
 
-const getTodos = (resource, callback) => {
-  const request = new XMLHttpRequest();
+const getTodos = (resource) => {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
 
-  request.addEventListener('readystatechange', () => {
-    // console.log(request, request.readyState);
-    if (request.readyState === 4 && request.status === 200) {
-      const data = JSON.parse(request.responseText);
-      callback(undefined, data);
-    } else if (request.readyState === 4) {
-      callback('could not fetch data', undefined);
-    }
+    request.addEventListener('readystatechange', () => {
+      if (request.readyState === 4 && request.status === 200) {
+        const data = JSON.parse(request.responseText);
+        resolve(data);
+      } else if (request.readyState === 4) {
+        reject('error getting resource');
+      }
+    });
+
+    request.open('GET', resource);
+    request.send();
   });
-
-  request.open('GET', resource);
-  request.send();
 };
+
+getTodos('todos/luigis.json')
+  .then((data) => {
+    console.log('promise resolved:', data);
+  })
+  .catch((err) => {
+    console.log('promise rejected:', err);
+  });
