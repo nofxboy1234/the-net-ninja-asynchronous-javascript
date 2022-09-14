5ec3e027f007d0dd5385fffbae5496bb2f3540b0
Dylan Palmboom <nofxboy1234@gmail.com>
2022-09-14 14:13:16 +0200

    Chain Promises together

--- index.js
+++ index.js
@@ -55,9 +55,18 @@ const getTodos = (resource) => {
   });
 };
 
-getTodos('todos/luigis.json')
+getTodos('todos/luigi.json')
   .then((data) => {
-    console.log('promise resolved:', data);
+    console.log('promise 1 resolved:', data);
+    return getTodos('todos/mario.json');
+  })
+  .then((data) => {
+    // Fired when 'todos/mario.json is complete
+    console.log('promise 2 resolved:', data);
+    return getTodos('todos/shaun.json');
+  })
+  .then((data) => {
+    console.log('promise 3 resolved:', data);
   })
   .catch((err) => {
     console.log('promise rejected:', err);
